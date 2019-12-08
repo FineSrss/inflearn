63fdccb0b2628ac04ac1128f8cc4c22a3db3c5fa
FineSrss <mrk3927@gmail.com>
2019-12-08 18:04:32 +0900

    howlstagram create main nav tab bar

---
+++ howlstagram/app/src/main/java/demo/howl/howlstagramf16/navigation/GridFragment.kt
@@ -0,0 +1,15 @@
+package demo.howl.howlstagramf16.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import demo.howl.howlstagramf16.R
+
+class GridFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid, container, false)
+        return view
+    }
+}
